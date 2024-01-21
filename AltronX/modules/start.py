204fc327c83710706e6c9276d502d7a43268f0ba
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-01-21 23:50:30 +0530

    Update start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -1,5 +1,5 @@
 from telethon import events, Button
-from config import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10, MK11, MK12, MK13, MK14, MK15
+from config import MK1, MK2, MK3, MK4, MK5 , MK6, MK7, MK8, MK9, MK10, MK11, MK12, MK13, MK14, MK15, MK16, MK17, MK18, MK19, MK20, MK21, MK22, MK23, MK24, MK25, MK26, MK27, MK28, MK29, MK30, MK31, MK32, MK33, MK34, MK35, MK36, MK37, MK38, MK39, MK40, MK41, MK42, MK43, MK44, MK45, MK46, MK47, MK48, MK49, MK50 
 from AltronX.modules.help import *
 import telethon
 
@@ -33,6 +33,42 @@ PythonButton = [
 @MK13.on(events.NewMessage(pattern="/start"))
 @MK14.on(events.NewMessage(pattern="/start"))
 @MK15.on(events.NewMessage(pattern="/start"))
+@MK16.on(events.NewMessage(pattern="/start"))
+@MK16.on(events.NewMessage(pattern="/start"))
+@MK17.on(events.NewMessage(pattern="/start"))
+@MK18.on(events.NewMessage(pattern="/start"))
+@MK19.on(events.NewMessage(pattern="/start"))
+@MK20.on(events.NewMessage(pattern="/start"))
+@MK21.on(events.NewMessage(pattern="/start"))
+@MK22.on(events.NewMessage(pattern="/start"))
+@MK23.on(events.NewMessage(pattern="/start"))
+@MK24.on(events.NewMessage(pattern="/start"))
+@MK25.on(events.NewMessage(pattern="/start"))
+@MK26.on(events.NewMessage(pattern="/start"))
+@MK27.on(events.NewMessage(pattern="/start"))
+@MK28.on(events.NewMessage(pattern="/start"))
+@MK29.on(events.NewMessage(pattern="/start"))
+@MK30.on(events.NewMessage(pattern="/start"))
+@MK31.on(events.NewMessage(pattern="/start"))
+@MK32.on(events.NewMessage(pattern="/start"))
+@MK33.on(events.NewMessage(pattern="/start"))
+@MK34.on(events.NewMessage(pattern="/start"))
+@MK35.on(events.NewMessage(pattern="/start"))
+@MK36.on(events.NewMessage(pattern="/start"))
+@MK37.on(events.NewMessage(pattern="/start"))
+@MK38.on(events.NewMessage(pattern="/start"))
+@MK39.on(events.NewMessage(pattern="/start"))
+@MK40.on(events.NewMessage(pattern="/start"))
+@MK41.on(events.NewMessage(pattern="/start"))
+@MK42.on(events.NewMessage(pattern="/start"))
+@MK43.on(events.NewMessage(pattern="/start"))
+@MK44.on(events.NewMessage(pattern="/start"))
+@MK45.on(events.NewMessage(pattern="/start"))
+@MK46.on(events.NewMessage(pattern="/start"))
+@MK47.on(events.NewMessage(pattern="/start"))
+@MK48.on(events.NewMessage(pattern="/start"))
+@MK49.on(events.NewMessage(pattern="/start"))
+@MK50.on(events.NewMessage(pattern="/start"))
 async def start(event):              
     if event.is_private:
         AltBot = await event.client.get_me()
@@ -47,4 +83,4 @@ async def start(event):
                 "https://telegra.ph/file/911bc5ee7330f9dc72ee8.jpg",
                 caption=TEXT, 
                 buttons=PythonButton)
-            
+
