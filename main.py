20f67ad90b0dd97778c4c826c627bad74ad08981
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-01-22 01:28:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from utils import load_plugins
 import logging
 import asyncio
-from config import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10
+from config import MK1, MK2, MK3, MK4, MK5 , MK6, MK7, MK8, MK9, MK10, MK11, MK12, MK13, MK14, MK15, MK16, MK17, MK18, MK19, MK20, MK21, MK22, MK23, MK24, MK25, MK26, MK27, MK28, MK29, MK30, MK31, MK32, MK33, MK34, MK35, MK36, MK37, MK38, MK39, MK40, MK41, MK42, MK43, MK44, MK45, MK46, MK47, MK48, MK49, MK50 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -17,7 +17,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑 𝐒𝐩𝐚𝐦 𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 😎🤘🏻\nMy Master ---> @II_BAD_BBY_II")
+print("\𝗟𝗘𝗚𝗘𝗡𝗗 𝗦𝗣𝗔𝗠 𝗕𝗢𝗧 𝗗𝗘𝗣𝗟𝗢𝗬𝗘𝗗  𝗦𝗨𝗖𝗖𝗘𝗦𝗙𝗨𝗟𝗟𝗬😎🤘🏻\n𝗠𝗬 𝗗𝗔𝗗 ---> @II_BAD_BBY_II")
 
 
 async def main():
@@ -31,6 +31,47 @@ async def main():
     await MK8.run_until_disconnected()
     await MK9.run_until_disconnected()
     await MK10.run_until_disconnected()
+    await MK11.run_until_disconnected()
+    await MK12.run_until_disconnected()
+    await MK13.run_until_disconnected()
+    await MK14.run_until_disconnected()
+    await MK15.run_until_disconnected()
+    await MK16.run_until_disconnected()
+    await MK17.run_until_disconnected()
+    await MK18.run_until_disconnected()
+    await MK19.run_until_disconnected()
+    await MK20.run_until_disconnected()
+    await MK21.run_until_disconnected()
+    await MK22.run_until_disconnected()
+    await MK23.run_until_disconnected()
+    await MK24.run_until_disconnected()
+    await MK25.run_until_disconnected()
+    await MK26.run_until_disconnected()
+    await MK27.run_until_disconnected()
+    await MK28.run_until_disconnected()
+    await MK29.run_until_disconnected()
+    await MK30.run_until_disconnected()
+    await MK31.run_until_disconnected()
+    await MK32.run_until_disconnected()
+    await MK33.run_until_disconnected()
+    await MK34.run_until_disconnected()
+    await MK35.run_until_disconnected()
+    await MK36.run_until_disconnected()
+    await MK37.run_until_disconnected()
+    await MK38.run_until_disconnected()
+    await MK39.run_until_disconnected()
+    await MK40.run_until_disconnected()
+    await MK41.run_until_disconnected()
+    await MK42.run_until_disconnected()
+    await MK43.run_until_disconnected()
+    await MK44.run_until_disconnected()
+    await MK45.run_until_disconnected()
+    await MK46.run_until_disconnected()
+    await MK47.run_until_disconnected()
+    await MK48.run_until_disconnected()
+    await MK49.run_until_disconnected()
+    await MK50.run_until_disconnected()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
+  
